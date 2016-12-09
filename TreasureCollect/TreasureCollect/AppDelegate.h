894df2a4e01043d8ec6548aceba7142069e76bcb
Chenmb <15111151992@163.com>
2016-12-09 11:07:40 +0800

    创建项目

---
+++ TreasureCollect/TreasureCollect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TreasureCollect
+//
+//  Created by Apple on 2016/12/9.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
